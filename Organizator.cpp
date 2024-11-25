8634d2e257ff2b20e04e3d8e0e44e3083a4d2f73
let-lucasOliveira <lucas.nro1000@gmail.com>
2024-11-25 13:50:10 -0300

    :sparkles: Add stack menu :constructio: Initial queue menu

--- Organizator.cpp
+++ Organizator.cpp
@@ -4,6 +4,7 @@ Author: Lucas Oliveira
 Date: oct/26/2024
 Description: Um algoritmo para unir todas as funcionalidades ministradas na disciplina de Estrutura de Dados, planejada e ministrada por Alexandre Cassiano.
 Neste projeto abordaremos estruturas de dados como:
+
  - Lista
  - Pilha
  - Fila
@@ -12,6 +13,8 @@ Neste projeto abordaremos estruturas de dados como:
 
  - Bubble Sort ()
  - Selection Sort ()
+ - Insertion Sort ()
+ - Shell Sort ()
 
 */
 #include <stdio.h>
@@ -34,30 +37,39 @@ void clearPrompt(); // Limpa o prompt de comando atual
 int structureUsed[] = {0, 0, 0}; // Array binario para retornar qual strutura foi usada.
 
 // Sessao pilha
-int *stack = (int *)malloc(sizeof(stack) / sizeof(stack[0])); // Pilha global
+int *stack = (int *)malloc(sizeof(stack) / sizeof(int)); // Pilha global
 
-void initStack(); // Inicia a estrutura pilha
 void StackQuestion(); // Prompt para adicao ou remocao de dado na pilha
+void initStack(); // Inicia a estrutura pilha
 int pop(); // Assume o valor da ultima posicao adicionada na pilha
 void push(int num); // Adiciona um elemento na ultima posicao
-void printStack(); // Imprime a pilha
+void printAll(int *arr); // Imprime a pilha
 void binaryConversor(int decNum); // Funcao para conversao de um numero de base decimal para base binaria
 
 int peek = -1; // Topo da pilha
 
 // Sessao fila
-int *queue = (int *)malloc(sizeof(queue) / sizeof(queue[0])); // Fila global
+int *queue = (int *)malloc(sizeof(queue) / sizeof(int)); // Fila global
+int first = -1; // Variavel que armazena o indice do primeiro elemento da fila
 
 void QueueQuestion(); // Prompt da fila para adicicao ou remocao de dados
 
 // Sessao lista
 void ListQuestion(); // Prompt da lista encadeada, para adicao ou remocao de dados
 
+typedef struct {
+    int value;
+    int *next, *prev;
+} List;
+
 //--------------------Big O Notation----------------
 // :construction: Aqui a intencao eh que comparamos a eficiencia de dois algoritmos sort, usando o tempo de execucao e a quantidade de trocas e comparacao, para determinar a eficiencia e custo para o hardware
 
-int comp, trocas; // Variaveis que armazezam a quantidade de trocas e a quantidade que foram comparadas
-void compareSort(/*void sortMethod(), void sortMethod2(), int structureUsed[]*/);
+int comp, shift; // Variaveis que armazezam a quantidade de trocas e a quantidade que foram comparadas
+void compareSortQuestion(/*void sortMethod(), void sortMethod2(), int structureUsed[]*/);/* Linha 64
+* - Esta funcao void deve perguntar ao usuario dois metodos sort de ordenacao de dados (Estes nao podem se repetir, exemplo comparar um bubble sort com um bubble sort, ou qualquer outro). 
+* - Provavelmente a funcao compareSort(*sort1(), *sort2(), bigONotation) devera receber dois ponteiros como parametros e/ou uma funcao para comparar a eficiencia, nomeado de Big O Notation mas pode ser que seja alterado.
+*/
 
 //---------------------------------------------------
 
@@ -71,7 +83,7 @@ void initSelection(); // Inicia o selection sort
 // Sessao insertion sort
 
 // Sessao shell sort
-
+//
 
 /*************************** Fim da sessao de prototipacao **************************************************/
 
@@ -79,11 +91,11 @@ void initSelection(); // Inicia o selection sort
 
 int main() {
     if(stack == NULL){
-        printf("\n!!!! Memory allocation failure !!!!");
+        printf("\n!!!! Stack memory allocation failure !!!!");
         return 1;
     }
     if (queue == NULL){
-       printf("\n!!!! Memory allocation failure !!!!");
+       printf("\n!!!! Queue memory allocation failure !!!!");
         return 1;
     }
 	DSQuestion();
@@ -96,7 +108,7 @@ int main() {
 
 /**************************** Sessao de Modelagem de funcao ************************************************/
 void DSQuestion(){
-	comp = 0, trocas = 0;
+	comp = 0, shift = 0;
     int option = 0;
     puts("==========================");
     puts("Bem vindo ao programa!");
@@ -104,13 +116,13 @@ void DSQuestion(){
     scanf("%d", &option);
     switch (option) {
         case 1: // Lista
-            structureList();
+            ListQuestion();
             break;
         case 2: // Fila
-            structureQueue();
+            QueueQuestion();
             break;
         case 3: // Pilha
-            structureStack();
+            StackQuestion();
             break;
         case 4: // Ordenacao de dados
             structQuestion();
@@ -134,7 +146,7 @@ void structQuestion(){
 
     while(1){
         clearPrompt();
-        printf("\nPor favor escolha qual estrutura voce gostaria que voce ordenada:\n");
+        printf("\nPor favor escolha qual estrutura voce gostaria que fosse ordenada:\n");
         if(structureUsed[0]){
             printf("1 - Pilha\n");
             scanf("%d", &opt);
@@ -152,19 +164,19 @@ void structQuestion(){
         
         switch(opt){
             case 1: // Stack
-                if (structureUsed[0] == 1)
+                if (structureUsed[0])
                     sortQuestion(0);
                 else
-                    printf("Pilha vazia...\nOcao invalida!\n");
+                    printf("Pilha vazia...\nOpcao invalida!\n");
                 break;
             case 2: // Queue
-                if (structureUsed[1] == 1)
+                if (structureUsed[1])
                     sortQuestion(1);
                 else
                     printf("Fila vazia...\nOpcao invalida!\n");
                 break;
             case 3: // List
-                if (structureUsed[2] == 1)
+                if (structureUsed[2])
                     sortQuestion(2);
                 else
                     printf("Lista vazia...\nOpcao invalida!\n");
@@ -185,7 +197,6 @@ void sortQuestion(int strChose){
     int opt = strChose;
     int chose;
     int *auxArray = (int *)malloc(sizeof(auxArray) / sizeof(auxArray[0])); // Aqui criamos um bloco de memoria continuo, se comporta semelhante a um array, mas deixa de ser um... Então podemos nos referir aos proximos blocos como auxArray[]
-    // int size = sizeof(auxArray) / 
 
     // Aqui a intencao eh criar um array auxiliar que ira armazenar as informacoes de acordo com a escolha de estrutura do usuario
     // Nao ha valor default porque ira receber da structQuestion (Precisa verificar se uma func void pode ser recursivaw)
@@ -196,12 +207,12 @@ void sortQuestion(int strChose){
         }
         break;
     case 1: // Queue
-        for(int j = 0; j < peek; j++){
+        for(int j = 0; j < first /* Aqui eh apenas uma variavel tipo o pico de uma fila, mas este eh para fila */; j++){
             auxArray[j] = queue[j];
         }
         break;
     case 2: // List
-
+            auxArray[j] ;// Ira receber o valor da lista, provavelmente terá que percorrer todas até que o valor de "Prox" seja NULL.
         break;
     case 3: // random numbers
         RandomizeArray(auxArray, 1000); // Esta funcao deve randomizar o array deste escopo
@@ -209,14 +220,14 @@ void sortQuestion(int strChose){
     }
 
 
-    while(10){
+    while(1){
         printf("\nEscolha como devemos organizar esta estrutura:\n1 - Comparar dois metodos\n2 - Bubble sort\n3 - Selection sort\n4 - Insertion sort\n5 - Shell sort");
         printf("\n0 - Voltar...");
         scanf("%d", chose);
 
         switch(chose){
             case 1:
-                compareSort();
+                compareSortQuestion(); // Aqu
                 break;
             case 2:
                 initBuble();
@@ -247,7 +258,7 @@ void structureStack() {
         int option = 0;
         clearPrompt();
 
-        printf("\nPilha atual:  "); printfStack();
+        printf("\nPilha atual:  "); printAll(stack);
         printf("\n\n1 - Inserir na pilha\n2 - Retirar da pilha\n3 - Conversor binario\n5 - Finalizar");
         printf("\nOpcao:	"); scanf("%d", &option);
 
@@ -257,8 +268,8 @@ void structureStack() {
                     int newValue = 0;
                     clearPrompt();
 
-                    printf("\nPilha atual:  "); printfStack();
-                    printf("\n\nDigite um valor que deseja inserir: \nDigite 0 para parar de adicionar..."); scanf("%d", &newValue); 
+                    printf("\nPilha atual:  "); printAll(stack);
+                    printf("\n\nDigite um valor inteiro que deseja inserir: \nDigite 0 para parar de adicionar..."); scanf("%d", &newValue); 
                     if(newValue == 0)
                         break;
                     else
@@ -272,7 +283,9 @@ void structureStack() {
                     structureUsed[0] = 0; // Atualiza o array informando que a estrutura pilha agora esta vazia
                 }else
                     clearPrompt();
-                    printf("%d Retirado!\n",pop());
+                    peek = -1 ? printf("Pilha vazia") : printf("%d Retirado!\n",pop());
+
+                    printf("Digite qualquer tecla para continuar..."); getchar();
                 break;
             case 3: 
                 elem = 0;
@@ -289,11 +302,12 @@ void structureStack() {
     }
 }
 
-void printStack() {
-    if(peek == -1)
-        printf("Vazia!\n");
+void printAll(int *arr) {
+    int size = sizeof(arr) / sizeof(int);
+    if(size || NULL  == 0) // <!-- Verificar se vai dar certo, pode ser que ocorra algum erro de sintax -->
+        printf("Vazia!\n"); 
     else
-        for(int in = 0; in < peek; in++)
+        for(int in = 0; in < size; in++)
             printf("%d", stack[in]);
 }
 
@@ -305,14 +319,43 @@ int pop(){
 }
 
 void push(int num){
-        ++peek;
-        stack[peek] = num;
+    stack[++peek] = num;
 }
 
 void binaryConversor(int decNum){
     clearPrompt();
 
     printf("\n= = = = = = = = = EM CONSTRUCAO = = = = = = = = = =");
+}
+
+void QueueQuestion(){
+    int opt = NULL;
+
+    while(1){
+        clearPrompt();
+        printf("Valor da fila:  "); printAll(queue);
+        printf("\nO que deseja fazer:\n1 - Inserir valores na fila\n2 - Retirar valores da fila\n0 - Finalizar");
+        structureUsed[0] == 1 ? printf("\n3 - Importar valores da pilha (Valores estarão na ordem inversa!)") : printf("");
+        
+        scanf("%d", &opt);
+
+
+        switch(opt){
+            case 1: // Inserir
+            break;
+            case 2: // Retirar
+            break;
+            case 3: // Importar da stack
+            break;
+            case 0:
+                DSQuestion();
+            break;
+            default:
+                printf("\nOpcao invalida...");
+            break;
+        }
+    }
+    
 }
 
 // Funcao para somar os valores que estao armazenados no array
